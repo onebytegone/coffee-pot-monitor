759e3130571e9aad870097f283e14d0d8dfeb419
Ethan Smith <ethan@onebytegone.com>
2025-08-24 12:40:51 -0400

    fix: include api domain in the web jwt

--- bin/register-device.sh
+++ bin/register-device.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-set -eou pipefail
+set -eo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-DEVICE_ID=$(uuidgen | tr '[:upper:]' '[:lower:]')
+API_DOMAIN=$1
+DEVICE_ID=${2:-$(uuidgen | tr '[:upper:]' '[:lower:]')}
 
-WEB_TOKEN_PAYLOAD='{"authorization_details":[{"deviceID":"'"${DEVICE_ID}"'","actions":["read"]}]}'
+if [[ -z "${API_DOMAIN}" || -z "${DEVICE_ID}" ]]; then
+   echo "Usage: $0 <api-domain> [<device-id>]" >&2
+   exit 1
+fi
+
+WEB_TOKEN_PAYLOAD='{"aud":"'"${API_DOMAIN}"'","authorization_details":[{"deviceID":"'"${DEVICE_ID}"'","actions":["read"]}]}'
 DEVICE_TOKEN_PAYLOAD='{"sub":"'"${DEVICE_ID}"'"}'
+
+echo "API URL: ${API_DOMAIN}"
+echo "Device ID: ${DEVICE_ID}"
+echo
 
 echo "Web UI Token:"
 echo
