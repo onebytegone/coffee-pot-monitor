c9862d24619031d7676236140ddac1aaef603d28
Ethan Smith <ethan@onebytegone.com>
2025-08-24 12:40:22 -0400

    style: autoformat

--- api/src/middleware/validate-token.ts
+++ api/src/middleware/validate-token.ts
@@ -2,7 +2,10 @@ import { IRequestStrict, RequestHandler, error } from 'itty-router';
 import { importSPKI, JWTPayload, jwtVerify } from 'jose';
 import { CloudFlareArgs } from '../worker-types';
 
-export const validateToken: RequestHandler<IRequestStrict & { token: JWTPayload }, CloudFlareArgs> = async (request, env) => {
+export const validateToken: RequestHandler<
+   IRequestStrict & { token: JWTPayload },
+   CloudFlareArgs
+> = async (request, env) => {
    const rawToken = request.headers.get('Authorization')?.replace('Bearer ', '');
 
    if (!rawToken) {
@@ -11,10 +14,10 @@ export const validateToken: RequestHandler<IRequestStrict & { token: JWTPayload 
 
    try {
       const publicKey = await importSPKI(env.PUBLIC_KEY, 'RS256'),
-            result = await jwtVerify(rawToken, publicKey, { algorithms: [ 'RS256' ] });
+         result = await jwtVerify(rawToken, publicKey, { algorithms: ['RS256'] });
 
       request.token = result.payload;
    } catch (err) {
       return error(403);
    }
-}
+};
